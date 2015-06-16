754f1448d45d60f7308213e8b5b1ad47db0c6062
mat10tng <mat10tng@gmail.com>
2015-06-16 15:19:28 +0200

    fixing slide so its only slide at the edge

--- www/js/app.js
+++ www/js/app.js
@@ -12,13 +12,16 @@ angular.module('ionicApp', ['ionic'])
                 templateUrl:'templates/sign-in.html',
                 controller:'SignInCtrl'
             })
+         //   all the middle states here
 
-            // TODO
+            // this wrap the entire view port
             .state('main', {
                 url: "/main",
                 abstract: true,
                 templateUrl: "templates/event-menu.html"
             })
+
+            // we begin from this view.
             .state('main.home', {
                 url: "/home",
                 views: {
@@ -27,6 +30,9 @@ angular.module('ionicApp', ['ionic'])
                     }
                 }
             })
+
+            // TODO making more state
+
             .state('main.checkin', {
                 url: "/check-in",
                 views: {
@@ -45,7 +51,13 @@ angular.module('ionicApp', ['ionic'])
                     }
                 }
             })
-
+            /** Not done yet
+            .state('signout',{
+                url: "/sign-out",
+                templateUrl:'templates/sign-out.html',
+                controller:'SignInCtrl'
+            })
+            */
         $urlRouterProvider.otherwise("/sign-in");
     })
 
@@ -60,63 +72,21 @@ angular.module('ionicApp', ['ionic'])
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
     .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
-        $scope.attendees = [
-            { firstname: 'Nicolas', lastname: 'Cage' },
-            { firstname: 'Jean-Claude', lastname: 'Van Damme' },
-            { firstname: 'Keanu', lastname: 'Reeves' },
-            { firstname: 'Steven', lastname: 'Seagal' }
-        ];
 
-        $scope.toggleLeft = function() {
-            $ionicSideMenuDelegate.toggleLeft();
-        };
+
+       /** $scope.toogleRight = function() {
+            $ionicSideMenuDelegate.toogleRight();
+        };*/
     })
 
     .controller('CheckinCtrl', function($scope) {
-        $scope.showForm = true;
 
-        $scope.shirtSizes = [
-            { text: 'Large', value: 'L' },
-            { text: 'Medium', value: 'M' },
-            { text: 'Small', value: 'S' }
-        ];
-
-        $scope.attendee = {};
-        $scope.submit = function() {
-            if(!$scope.attendee.firstname) {
-                alert('Info required');
-                return;
-            }
-            $scope.showForm = false;
-            $scope.attendees.push($scope.attendee);
-        };
 
     })
 
     .controller('AttendeesCtrl', function($scope) {
 
-        $scope.activity = [];
-        $scope.arrivedChange = function(attendee) {
-            var msg = attendee.firstname + ' ' + attendee.lastname;
-            msg += (!attendee.arrived ? ' has arrived, ' : ' just left, ');
-            msg += new Date().getMilliseconds();
-            $scope.activity.push(msg);
-            if($scope.activity.length > 3) {
-                $scope.activity.splice(0, 1);
-            }
-        };
+
 
     });
